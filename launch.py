1d11203ac704f5b256e6a6a999ba96e6f04bf5fb
Tom Connolly <tom@tconnolly.co.uk>
2018-11-01 22:43:50 +0000

    restructuring

--- launch.py
+++ launch.py
@@ -3,11 +3,6 @@ from birthday.client import Client
 
 logging.basicConfig(level=logging.INFO)
 
-
-def main():
+if __name__ == '__main__':
     client = Client()
     client.run('NTA0MjQyMjkxMTk2NjkwNDQ0.DrCL5g.7AKoVGHQIUKzFJhkEetQefE3FSg')
-
-
-if __name__ == '__main__':
-    main()
